3001ba70cf7be23815a6a1257a012e27bb2d589b
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-10-29 12:02:07 +0530

    define changeset in people schema

--- lib/friends/person.ex
+++ lib/friends/person.ex
@@ -6,4 +6,10 @@ defmodule Friends.Person do
     field :last_name, :string
     field :age, :integer
   end
+
+  def changeset(person, attrs) do
+    person
+    |> Ecto.Changeset.cast(attrs, [:first_name, :last_name, :age])
+    |> Ecto.Changeset.validate_required([:first_name, :last_name])
+  end
 end
