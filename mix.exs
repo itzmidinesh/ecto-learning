f18863aaab66fed734e94798b1a00a9a206f96af
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-11-01 13:24:38 +0530

    setup alias and remove from repo case

--- mix.exs
+++ mix.exs
@@ -6,7 +6,9 @@ defmodule Friends.MixProject do
       app: :friends,
       version: "0.1.0",
       elixir: "~> 1.17",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -19,6 +21,10 @@ defmodule Friends.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -27,6 +33,21 @@ defmodule Friends.MixProject do
       {:jason, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      setup: ["deps.get", "ecto.setup"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
--- test/support/repo_case.ex
+++ test/support/repo_case.ex
@@ -10,10 +10,4 @@ defmodule Friends.RepoCase do
       import Friends.RepoCase
     end
   end
-
-  setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Friends.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
-    :ok
-  end
 end
